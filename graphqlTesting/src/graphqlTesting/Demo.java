5d244ce1f103bfaeacc844108ac3d41c34cd18b9
kasun <kasunsiyambalapitiya@gmail.com>
2017-02-14 09:32:34 +0530

    tenth commit

--- graphqlTesting/src/graphqlTesting/Demo.java
+++ graphqlTesting/src/graphqlTesting/Demo.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) Feb 14, 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ *  Copyright (c) Feb 14, 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 
 
@@ -28,6 +28,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -38,6 +39,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONArray;
@@ -107,10 +110,10 @@ public class Demo {
 
         setURL("http://umt.private.wso2.com:9765/codequalitymatricesapi/1.0.0//properties?path=/_system/governance/patchs/"+getPatchId());
 
-//        callingTheAPI(urlForObtainingCommitHashes,jsonOutPutFileOfCommits,true,false,false);
+        //        callingTheAPI(urlForObtainingCommitHashes,jsonOutPutFileOfCommits,true,false,false);
 
     }
-    
+
 
     //=========== calling the relevant API and saving the output to a file===============================================
 
@@ -209,7 +212,7 @@ public class Demo {
 
         }
     }
-    
+
     //  ===================== getting the commit IDs from the above saved file ============================================
 
     public void getThePublicGitCommitId(){
@@ -298,7 +301,7 @@ class GettingBlameCommit extends Demo {
     public void setUrlForGetingFilesChanged(String repoName,String commitHash) {
         this.urlForGetingFilesChanged ="http://api.github.com/repos/"+repoName+"/commits/"+commitHash;
     }
-    
+
     //================ obtaining PR for each commit and saving them in a file ===================================
     public void obtainingRepoNamesForCommitHashes() throws IOException{
 
@@ -313,8 +316,8 @@ class GettingBlameCommit extends Demo {
             saveRepoNamesInAnArray(commitHash);
 
         }
-        
-        
+
+
 
 
 
@@ -399,7 +402,7 @@ class GettingBlameCommit extends Demo {
 
 
     }
-    
+
     public void callingToGetFilesChanged(String repoLocation, String commitHash){
 
         //        setting the URL for calling github single commit API
@@ -526,7 +529,7 @@ class GettingBlameCommit extends Demo {
 
 
     }
-    
+
     //============================ iterating and calling graphql==================================================
     public void iteratingOver(String repoLocation, String commitHash){
 
@@ -546,19 +549,19 @@ class GettingBlameCommit extends Demo {
 
             graphqlApiJsonObject.put("query","{repository(owner:\""+owner+"\",name:\""+repositoryName+"\"){object(expression:\""+commitHash+"\"){ ... on Commit{blame(path:\""+fileName+"\"){ranges{startingLine endingLine age commit{history(first: 2) { edges { node {  message url } } } author { name email } } } } } } } }");
 
-//            try{
-//                //            calling the graphql API for getting blame information for the current file and saving it in a location.
-//                locationOfTheSavedFile= callingGraphQl(graphqlApiJsonObject,fileName,commitHash,repoLocation);
-//
-//            }
-//            catch(IOException e){
-//                e.printStackTrace();
+            try{
+                //            calling the graphql API for getting blame information for the current file and saving it in a location.
+                locationOfTheSavedFile= callingGraphQl(graphqlApiJsonObject,fileName,commitHash,repoLocation);
+
+            }
+            catch(IOException e){
+                e.printStackTrace();
             }
             //            reading the above saved output for the current file name
 
 
 
-//            readingTheBlameReceived(locationOfTheSavedFile,fileName,owner,repositoryName,repoLocation);
+            //            readingTheBlameReceived(locationOfTheSavedFile,fileName,owner,repositoryName,repoLocation);
 
 
 
@@ -569,6 +572,92 @@ class GettingBlameCommit extends Demo {
 
 
     }
+    public String callingGraphQl(JSONObject queryObject,String fileName,String commitHash,String repoLocation) throws IOException{
 
+        CloseableHttpClient client= null;
+        CloseableHttpResponse response= null;
+
+
+        client= HttpClients.createDefault();
+        HttpPost httpPost= new HttpPost("https://api.github.com/graphql");
+
+        httpPost.addHeader("Authorization","Bearer "+getToken());
+        httpPost.addHeader("Accept","application/json");
+
+        try {
+
+            //                StringEntity entity= new StringEntity("{\"query\":\"query "+graphqlQuery+"\"}");
+            StringEntity entity= new StringEntity(queryObject.toString());
+
+
+            httpPost.setEntity(entity);
+            response= client.execute(httpPost);
+
+        }
+
+        catch(UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        catch(ClientProtocolException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+        BufferedWriter bufferedWritter=null;
+        BufferedReader bufferedReader=null;
+        String saveLocation=null;
+        try{
+            bufferedReader= new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            String line= null;
+            StringBuilder stringBuilder= new StringBuilder();
+            while((line=bufferedReader.readLine())!= null){
+
+                stringBuilder.append(line);
+
+            }
+
+            //renaming the file .java to _java as it may conflict with the .json extension (if not will result in 2 extensions for the same file)
+            String fileNameWithoutExtension= StringUtils.substringBefore(fileName, ".");
+            String fileNamesExtension= StringUtils.substringAfter(fileName,".");
+
+            String modifiedFileName= fileNameWithoutExtension+"_"+fileNamesExtension;
+
+
+            // saving the output received to a file located in the same repo directory under the same owner of the repo
+            saveLocation= repoLocation+"/FileChanged/"+commitHash+"/"+modifiedFileName+"_blame.json";
+
+
+            File fileLocation= new File(location+saveLocation);
+            fileLocation.getParentFile().mkdirs();      //creating directories according to the file name given
+            bufferedWritter= new BufferedWriter(new FileWriter(fileLocation));
+
+            bufferedWritter.write(stringBuilder.toString());
+
+            System.out.println(stringBuilder.toString());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        finally{
+            if(bufferedWritter !=null){
+                bufferedWritter.close();
+
+            }
+            if(bufferedReader != null){
+                bufferedReader.close();}
+        }
+
+        return saveLocation;
+
+
+
+
+
+    }
+}
 
 
